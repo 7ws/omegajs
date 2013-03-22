969c9cfc295a621a462203f34022db9fa928b15c
Evandro Myller <eMyller@7ws.co>
2013-03-22 13:44:37 -0300

    [dom.Query] Basic functionality, selecting elements by tag name, id and class names.

--- omega.js
+++ omega.js
@@ -22,6 +22,11 @@ function _class(body) {
 	return init;
 }
 
+function _array(object) {
+	// convert a iterable object into an array
+	return Array.prototype.slice.call(object);
+}
+
 function _map(array, callback) {
 	// maps array with callback, returning the processed result
 	var results = [];
@@ -106,15 +111,69 @@ var dom = Omega.DOM = {
 						this.results.push(results[i]);
 			}
 
-			// process single selector
+			// process a single selector
 			else {
-				selector = selectors[0];
+				selector = selectors[0]
+					.replace(/\s+/g, ' ')
+					.replace(/([>+~]) /g, '$1')
+					.split(' ');
+
+				context = [document];
+
+				while (selector.length) {
+					var token = new dom.QueryToken(selector.shift());
+					var elements = [];
+
+					for (var i = -1, all = []; context[++i];)
+						_merge(all, _array(
+							context[i].getElementsByTagName(token.tag_name)));
+
+					for (var i = -1; all[++i];)
+						dom.match(all[i], token) && elements.push(all[i]);
+
+					context = elements;
+				}
+
+				this.results = elements;
 			}
 
 			// records the spent time
 			this.time = new Date - start_time;
 		}
-	})
+	}),
+
+	QueryToken: _class({
+		// parse a CSS selector into a detailed object
+
+		TAG: /^[a-z]+/i,
+		ID: /#[\w-]+/,
+		CLASS: /\.[\w-]+/g,
+
+		init: function (selector) {
+			this.tag_name = (selector.match(this.TAG) || ['*'])[0];
+			this.id = (selector.match(this.ID) || [null])[0];
+			this.classes = _array(selector.match(this.CLASS) || []);
+		}
+	}),
+
+	match: function (dom_element, token) {
+		// test an element against a QueryToken
+
+		// id
+		if (token.id && dom_element.getAttribute('id') !== token.id.slice(1))
+			return false;
+
+		// class name filtering
+		if (token.classes.length) {
+			var el_classes = ' ' + dom_element.className + ' ';
+
+			for (var i = -1; token.classes[++i];)
+				if (el_classes.indexOf(token.classes[i].slice(1)) < 0)
+					return false;
+		}
+
+		return true;
+	}
 };
 
 }();
